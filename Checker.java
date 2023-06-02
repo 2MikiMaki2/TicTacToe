b756d5a6dffd1638982c9980d8586f5e7149ed0a
Maksim Pavlicic <maksimpavlicic@gmail.com>
2023-06-01 22:50:10 -0700

    Methods to Checker to help with Computer logic

--- Checker.java
+++ Checker.java
@@ -1,14 +1,14 @@
 public class Checker {
     
     // Checks if the given location on the board is empty
-    public boolean checkIsEmpty(String[][] board, int row, int col) {
+    public static boolean checkIsEmpty(String[][] board, int row, int col) {
         if (board[row][col] != " ") {
             return false;
         }
         return true;
     }
 
-    public boolean boardCheck(String[][] board) {
+    public static boolean winCheck(String[][] board) {
         // Checks horizontally if someone won
         for (int r = 0; r < board.length; r++) {
             boolean horizontalSame = true;
@@ -46,6 +46,71 @@ public class Checker {
         }
         
         return false;
+    }
+
+    public static int hConsCheck(String[][] board, String letter) {
+        // Checks horizontally for two consecutive appearances of *letter*
+        int hIndex = 1;
+        for (int r = 0; r < board.length; r++) {
+            int hCount = 0;
+            for (int c = 0; c < board[r].length; c++) {
+                if (board[r][c].equals(letter)) {
+                    hCount++;
+                } else {
+                    hCount = 0;
+                }
+
+                if (hCount == 2) {
+                    return hIndex;
+                }
+
+                hIndex++;
+            }
+        }
+
+        return -1;
+    }
+
+    public static int vConsCheck(String[][] board, String letter) {
+        // Checks vertically for two consecutive appearances of *letter*
+        int vIndex = 1;
+        for (int c = 0; c < board[0].length; c++) {
+            int vCount = 0;
+            for (int r = 0; r < board.length; r++) {
+                if (board[r][c].equals(letter)) {
+                    vCount++;
+                } else {
+                    vCount = 0;
+                }
+
+                if (vCount == 2) {
+                    return vIndex;
+                }
+
+                vIndex+= 3;
+            }
+            vIndex-= 5;
+        }
+
+        return -1;
+    }
+
+    public static int dConsCheck(String[][] board, String letter) {
+        // Checks diagonals for two consecutive appearances of *letter*
+        int dCount = 0;
+        int dIndex = 1;
+        for (int i = 0; i < 3; i++) {
+            if (board[i][i].equals(letter)) {
+                dCount++;
+            }
+
+            if (dCount == 2) {
+                return dIndex;
+            }
+            dIndex += 4;
+        }
+
+        return -1;
     }
     
 }
--- ComputerPlayer.java
+++ ComputerPlayer.java
@@ -1,16 +1,27 @@
 /*
  * How will it interact with runner class to be assigned its letter?
  * How will it choose where to place its piece?
- * Computer logic: first blocks player, second tries to win, if can't do either makes random valid move.
+ * Computer logic: first tries winning move, then blocks player, if can't do either makes random valid move.
  */
 public class ComputerPlayer {
     private String letter;
+    
+    public ComputerPlayer() {
+        letter = "x";
+    }
+
+    public ComputerPlayer(String letter) {
+        this.letter = letter;
+    }
     
     // Computer makes a move by generating random number between 1 and 10
     // TODO: Code full computer logic as described above.
     public int compMove() {
         int randMove = 1 + (int) (Math.random() * 10);
         return randMove;
+
+        //First search if a winning move can be made
+
     }
 
     public void setLetter(String letter) {

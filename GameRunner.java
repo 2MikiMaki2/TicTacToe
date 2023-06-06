73f69d39a38ac76e7c158b02ce81c47023ab1485
Platypus-Chan <dylanjwang@outlook.com>
2023-06-06 13:48:31 -0700

    Hopefully this isn't a problem

--- GameRunner.java
+++ GameRunner.java
@@ -1,11 +1,10 @@
-import java.io.Console;
 import java.util.Scanner;
-
 public class GameRunner {
     
-    // TODO: Implement serialization (on what?)
-    private static boolean runGame(String s){
-        Scanner scan = new Scanner(System.in);
+    // Add try and except to check if player input is valid
+    // Implement serialization (on what?)
+    private static GameBoard runGame(String s){
+        Console console = System.console();
         GameBoard board = new GameBoard();
         ComputerPlayer ai = new ComputerPlayer();
 
@@ -40,7 +39,7 @@ public class GameRunner {
                 System.out.println(board);
                 if (Checker.winCheck(board.getBoard())){
                     System.out.println("You win! Good game.");;
-                    return true;
+                    return board;
                 }
             }
 
@@ -51,43 +50,61 @@ public class GameRunner {
                 System.out.println(board);
                 if (Checker.winCheck(board.getBoard())){
                     System.out.println("The computer wins! Good game.");
-                    return true;
+                    return board;
                 }
             }
         }
         System.out.println ("Nobody wins, it's a draw!");
         scan.close();
-        return false;
+        return board;
     }
 
     public static void main(String[] args){
-        Scanner scan = new Scanner(System.in);
+        Console console = System.console();
         
-        System.out.println("Hi! Welcome to Tic-Tac-Toe! Do you wanna play a game?");
-        System.out.println("Type 'Y' for yes, or type 'N' for no!");
-        String response = scan.nextLine();
-        response.toLowerCase();
-        
-        switch (response){
-            case "y":
-                System.out.println("Alright, let's play!");
-                String response2 = "";
-                int exOrOh = (int)(Math.random() * 10 + 1);
-                
-            if (exOrOh > 0 && exOrOh <= 5){
-                    response2 ="x";
-                }
-                if (exOrOh > 5 && exOrOh <= 10){
-                    response2 = "o";
-                }
-                runGame(response2);
-                
-                break;
+        System.out.println("Hi! Welcome to Tic-Tac-Toe!");
+        while (true){
+            System.out.println("Main Menu");
+            System.out.println("\tType 'N' for a new game");
+            System.out.println("\tType 'V' to view a saved game");
+            System.out.println("\tType 'X' to exit the game");
             
-            case "n":
-                System.out.println("Fine! See you next time!");
-                break;
-        }   
-        scan.close();
+            String response = scan.nextLine();
+            switch (response.toUpperCase()){
+                case "N":
+                    System.out.println("Alright, let's play!");
+                    String response2 = "";
+                    int exOrOh = (int)(Math.random() * 10 + 1);
+                    
+                    if (exOrOh > 0 && exOrOh <= 5){
+                        response2 ="x";
+                    }
+                    if (exOrOh > 5 && exOrOh <= 10){
+                        response2 = "o";
+                    }
+                    GameBoard board = runGame(response2);
+                    System.out.println ("That was a good game! Enter a file name to save this game! Otherwise, press the 'enter' key to skip this step!");
+                    String fileName = scan.nextLine();
+                    if (!fileName.equals("")){
+                        board.save(fileName);
+                    }
+                    break;
+                
+                case "X":
+                    System.out.println("Fine! See you next time!");
+                    scan.close();  
+                    return;
+
+                case "V":
+                    System.out.println("Please enter the file name!");
+                    fileName = scan.nextLine();
+                    GameBoard g = GameBoard.load(fileName);
+                    if (g != null){
+                        System.out.println(g);
+                    }
+                    break;
+            } 
+        }  
+      
     }
 }
